a75532adcdecf7f1de4e41424d4c1d33955b7e4f
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-09-09 22:53:49 +0200

    package with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name="MVim",
-    version="0.1.0",
-    author="Élie Bouttier",
-    author_email="elie+mvim@bouttier.eu.org",
-    packages=["mvim"],
-    url="http://cgit.bouttier.eu.org/mvim/",
-    license="LICENSE",
-    description="Rename or move files by editing their names with vim.",
-    long_description=open("README.md").read(),
-    entry_points={
-        "console_scripts": [
-            "mvim = mvim.main:main",
-        ],
-    },
-)
+setup()
